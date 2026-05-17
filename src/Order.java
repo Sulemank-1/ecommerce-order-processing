4cffda7d9d954a1540414a0376fcbd0387e22082
Sulemank-1 <suleman.dev42@gmail.com>
2026-05-18 02:18:50 +0500

    Add loadReceipt() to Order class
    This method loads saved receipts from a csv file

--- src/Order.java
+++ src/Order.java
@@ -1,7 +1,9 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Order {
     //Data Fields
@@ -64,6 +66,43 @@ public class Order {
             }
         } catch (IOException ex) {
             System.out.println("Error: Could not save data to file");
+        }
+    }
+
+    public void loadReceipt(String filename) {
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("No existing receipt file found.");
+            return;
+        }
+
+        try (Scanner input = new Scanner(file)) {
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                String[] tokens = line.split(",");
+
+                String type = tokens[0];
+                int id = Integer.parseInt(tokens[1]);
+                String name = tokens[2];
+                double price = Double.parseDouble(tokens[3]);
+
+                switch (type) {
+                    case "PHYSICAL":
+                        double weight = Double.parseDouble(tokens[4]);
+                        double taxRate = Double.parseDouble(tokens[5]);
+                        addProduct(new PhysicalProduct(id, name, price, weight, taxRate));
+                        break;
+
+                    case "DIGITAL":
+                        addProduct(new DigitalProduct(id, name, price));
+                        break;
+                }
+            }
+            System.out.println("Receipt successfully loaded from " + filename);
+        } catch (InvalidProductException e) {
+            System.out.println("Loading Error: Encountered corrupted or illegal product parameters. " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("System Error: Critical failure parsing the receipt data. " + e.getMessage());
         }
     }
 
