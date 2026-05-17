a3ab7786e4b13ef0216e729502209b194a8239a8
Sulemank-1 <suleman.dev42@gmail.com>
2026-05-17 09:46:56 +0500

    Add PhysicalProduct class
    This class represents tangible goods

---
+++ src/PhysicalProduct.java
@@ -0,0 +1,17 @@
+public class PhysicalProduct extends Product{
+    //Data Fields
+    private double taxRate;
+
+    //Constructor
+    public PhysicalProduct(int productID, String name, double basePrice, double weight, double taxRate){
+        super(productID, name, basePrice, weight);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    public double getFinalPrice(){
+        double basePrice = super.getBasePrice();
+        return basePrice + (basePrice * taxRate);
+    }
+
+}
