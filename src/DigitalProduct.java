4e7cbc520c9a923457f041d08f6bdd48e4128b0c
Sulemank-1 <suleman.dev42@gmail.com>
2026-05-17 10:02:12 +0500

    Add DigitalProduct class
    This class represents digital goods

---
+++ src/DigitalProduct.java
@@ -0,0 +1,26 @@
+public class DigitalProduct extends Product{
+    //Data Fields
+    private String downloadLink;
+
+    //Constructor
+    public DigitalProduct(int productID, String name, double basePrice){
+        super(productID, name, basePrice, 0.0);
+        downloadLink = "www.downloadLink.com";
+    }
+
+    //Getter
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    //Methods
+    @Override
+    public double getFinalPrice(){
+        return getBasePrice();
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " Download Link: " + getDownloadLink();
+    }
+}
